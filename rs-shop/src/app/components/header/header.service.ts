42b1be790e1b851d015d0cb42a4de57e3d704d89
croftik <Chayka_Lory@mail.ru>
2021-09-07 13:03:45 +0300

    feat: add conditions for invisible block in header

--- rs-shop/src/app/components/header/header.service.ts
+++ rs-shop/src/app/components/header/header.service.ts
@@ -22,17 +22,42 @@ export class HeaderService {
   showPayment() {
     this.isExpandedPayment = !this.isExpandedPayment;
     this.statePayment = this.isExpandedPayment ? 'expandedPayment' : 'initialPayment';
+    if (this.isExpandedContacts) {
+      this.isExpandedContacts = false;
+      this.stateContacts = 'initialContacts';
+    }
+    else if (this.isExpandedAccount) {
+      this.isExpandedAccount = false;
+      this.stateAccount = 'initialContacts';
+    }
   }
 
   showContacts() {
     this.isExpandedContacts = !this.isExpandedContacts;
     this.stateContacts = this.isExpandedContacts ? 'expandedContacts' : 'initialContacts';
+    if (this.isExpandedPayment) {
+      this.isExpandedPayment = false;
+      this.statePayment = 'initialContacts';
+    }
+    else if (this.isExpandedAccount) {
+      this.isExpandedAccount = false;
+      this.stateAccount = 'initialContacts';
+    }
   }
 
   showAccountInfo() {
     this.isExpandedAccount = !this.isExpandedAccount;
     this.stateAccount = this.isExpandedAccount ? 'expandedAccount' : 'initialAccount';
+    if (this.isExpandedContacts) {
+      this.isExpandedContacts = false;
+      this.stateContacts = 'initialContacts';
+    }
+    else if (this.isExpandedPayment) {
+      this.isExpandedPayment = false;
+      this.statePayment = 'initialContacts';
+    }
   }
+  
 
 }
 
