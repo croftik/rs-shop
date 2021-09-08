9a06418d791416ddc6e06149ea90fd1e5cd92537
croftik <Chayka_Lory@mail.ru>
2021-09-09 00:39:45 +0300

    feat: add declarations and imports

--- rs-shop/src/app/app.module.ts
+++ rs-shop/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import Shop from './store/shop.state';
+import { environment } from 'src/environments/environment';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { GoodsComponent } from './components/goods/goods.component';
+import { OneGoodComponent } from './components/one-good/one-good.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +22,19 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     LoginFormComponent,
     CatalogComponent,
-    FooterComponent
+    FooterComponent,
+    GoodsComponent,
+    OneGoodComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsModule.forRoot([Shop], {
+      developmentMode: !environment.production,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
