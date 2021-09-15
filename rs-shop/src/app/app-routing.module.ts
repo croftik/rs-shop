f098035a4d3f8978f02902ce55e3423102ba5a30
croftik <Chayka_Lory@mail.ru>
2021-09-15 19:16:11 +0300

    feat: implement routes

--- rs-shop/src/app/app-routing.module.ts
+++ rs-shop/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
+import { GoodsComponent } from './components/goods/goods.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ':category', component: GoodsComponent },
+  { path: ':category/:good', component: GoodsComponent },
+  { path: ':category/:good/:id', component: DetailsComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
