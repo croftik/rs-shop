25cbaa630e3482c8d5a38c6f4eef34bc68f4781e
croftik <Chayka_Lory@mail.ru>
2021-09-15 19:20:37 +0300

    feat: add method

--- rs-shop/src/app/components/one-good/one-good.component.ts
+++ rs-shop/src/app/components/one-good/one-good.component.ts
@@ -5,23 +5,37 @@ import { IGoodItem } from 'src/app/models/goods.model';
 import Shop from 'src/app/store/shop.state';
 import { map } from 'rxjs/operators';
 import { OneGoodService } from './one-good.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SetDetails } from 'src/app/store/shop.actions';
+import { GoodService } from 'src/app/services/good/good.service';
 
 @Component({
   selector: 'app-one-good',
   templateUrl: './one-good.component.html',
   styleUrls: ['./one-good.component.scss']
 })
-export class OneGoodComponent implements OnInit {
+export class OneGoodComponent {
 
-  goods$: Observable<IGoodItem[]>
+  goods$: Observable<IGoodItem[]>;
 
-  constructor(private store: Store, public oneGoodService: OneGoodService) { 
+  category: string;
+
+  subCategory: string;
+
+  constructor(private store: Store, public goodService: GoodService, private activeRoute: ActivatedRoute, private router: Router) { 
     this.goods$ = this.store.select(Shop.goods).pipe(
       map(data => data)
-    )
+    );
+    this.category = this.activeRoute.snapshot.params.category;
+    this.subCategory = this.activeRoute.snapshot.params.good;
   }
 
-  ngOnInit() {
+  openDetailsPage(event: Event) {
+    const id = <string>(<HTMLElement>(event.target)).id.split('_')[1];
+    this.store.select(Shop.goods).subscribe(data => {
+      const currentGood = data.filter(element => element.id === id);
+      this.store.dispatch(new SetDetails(currentGood))
+    });
+    this.router.navigate([`${this.category}/${this.subCategory}/${id}`]);
   }
-
 }
