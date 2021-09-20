c11027ac25c177cc5a2133b249adf41593315256
croftik <Chayka_Lory@mail.ru>
2021-09-21 00:10:14 +0300

    fix: implement correct output for fio

--- rs-shop/src/app/components/order-form/order-form.service.ts
+++ rs-shop/src/app/components/order-form/order-form.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { HttpService } from 'src/app/services/http/http.service';
 import Shop from 'src/app/store/shop.state';
@@ -9,15 +10,16 @@ import Shop from 'src/app/store/shop.state';
 })
 export class OrderFormService {
 
-  constructor(private httpService: HttpService, private store: Store) { }
+  constructor(private httpService: HttpService, private store: Store, private router: Router) { }
 
   makeOrder(form: FormGroup) {
     const address = `г.${form.controls.city.value}, ул.${form.controls.street.value}, д.${form.controls.house.value}, п.${form.controls.entrance.value}, э.${form.controls.floor.value} кв.${form.controls.flat.value}`;
+    const fio = `${form.controls.surname.value} ${form.controls.name.value} ${form.controls.fathername.value}`
     const items = this.store.selectSnapshot(Shop.cartItems);
     const body = {
       "items": items,
       "details": {
-        "name": form.controls.name.value,
+        "name": fio,
         "address": address,
         "phone": form.controls.phone.value,
         "timeToDeliver": form.controls.date.value,
@@ -25,6 +27,7 @@ export class OrderFormService {
       }
     }
     this.httpService.postOrder(body).subscribe();
+    this.router.navigate(['wait-list']);
   }
 
 }
