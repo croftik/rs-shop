ebf12eb2573d585d3b9b5ffbbf5cf8afdd24fc38
croftik <Chayka_Lory@mail.ru>
2021-09-22 20:42:58 +0300

    fix: replace methods

--- rs-shop/src/app/services/good/good.service.ts
+++ rs-shop/src/app/services/good/good.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { SetGoods } from 'src/app/store/shop.actions';
 import Shop from 'src/app/store/shop.state';
+import { IGoodItem } from 'src/app/models/goods.model';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,42 @@ export class GoodService {
     this.categoryId = this.store.selectSnapshot(Shop.currentCategory).id;
     this.subCategoryId = this.store.selectSnapshot(Shop.currentSubCategory).id;
     this.store.dispatch(new SetGoods(this.categoryId, this.subCategoryId));
+  }
+
+  getSortingGoods(goods: IGoodItem[]): IGoodItem[] {
+    const direction = this.store.selectSnapshot(Shop.isUp);
+    const typeOfSorting = this.store.selectSnapshot(Shop.typeOfSorting);
+    const items = [...goods];
+    if (direction) {
+      switch (typeOfSorting) {
+        case 'price': return items.sort(this.getSortingByPriceUp);
+        case 'rating': return items.sort(this.getSortingByRatingUp);
+        default: break;
+      }
+    } else {
+      switch (typeOfSorting) {
+        case 'price': return items.sort(this.getSortingByPriceDown);
+        case 'rating': return items.sort(this.getSortingByRatingDown);
+        default: break;
+      }
+    }
+    return goods;
+  }
+
+  getSortingByPriceUp(a: IGoodItem, b: IGoodItem): number {
+    return a.price - b.price;
+  }
+
+  getSortingByPriceDown(a: IGoodItem, b: IGoodItem): number {
+    return b.price - a.price;
+  }
+
+  getSortingByRatingUp(a: IGoodItem, b: IGoodItem): number {
+    return a.rating - b.rating;
+  }
+
+  getSortingByRatingDown(a: IGoodItem, b: IGoodItem): number {
+    return b.rating - a.rating;
   }
 
 }
