91f65d3842d5f0d9f67917dc03ce8b2f9aa24264
croftik <Chayka_Lory@mail.ru>
2021-09-23 16:54:04 +0300

    feat: add delay

--- rs-shop/src/app/components/order-form/order-form.service.ts
+++ rs-shop/src/app/components/order-form/order-form.service.ts
@@ -15,7 +15,7 @@ export class OrderFormService {
   constructor(private httpService: HttpService, private store: Store, private navService: NavigationService) { }
 
   makeOrder(form: FormGroup) {
-    const address = `г.${form.controls.city.value}, ул.${form.controls.street.value}, д.${form.controls.house.value}, п.${form.controls.entrance.value}, э.${form.controls.floor.value} кв.${form.controls.flat.value}`;
+    const address = `г.${form.controls.city.value}, ул.${form.controls.street.value}, д.${form.controls.house.value}, п.${form.controls.entrance.value}, э.${form.controls.floor.value}, кв.${form.controls.flat.value}`;
     const fio = `${form.controls.surname.value} ${form.controls.name.value} ${form.controls.fathername.value}`
     const items = this.store.selectSnapshot(Shop.cartItems);
     const body = {
@@ -31,6 +31,6 @@ export class OrderFormService {
     const token = <string>localStorage.getItem('user');
     this.httpService.postOrder(body).subscribe();
     this.store.dispatch(new SetToken(token));
-    this.navService.navigateToMainPage();
+    setTimeout(() => this.navService.navigateToWailList(), 1000);
   }
 }
